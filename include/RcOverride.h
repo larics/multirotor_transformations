b58349d698d9c0a31246f7625e235d606552bf9c
Lovro Markovic <lovro.markovic@fer.hr>
2019-02-08 13:28:55 +0100

    Start RC override implementation

---
+++ include/RcOverride.h
@@ -0,0 +1,16 @@
+/*
+ * RcOverride.h
+ *
+ *  Created on: Feb 8, 2019
+ *      Author: lmark
+ */
+
+#ifndef RCOVERRIDE_H_
+#define RCOVERRIDE_H_
+
+class RcOverride
+{
+
+};
+
+#endif /* RCOVERRIDE_H_ */
