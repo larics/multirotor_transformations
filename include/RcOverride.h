dd1d5db15db306d6fd1a6470e67f587168d56e13
Lovro Markovic <lovro.markovic@fer.hr>
2019-02-09 10:35:25 +0100

    Continue with implementation

--- include/RcOverride.h
+++ include/RcOverride.h
@@ -8,9 +8,19 @@
 #ifndef RCOVERRIDE_H_
 #define RCOVERRIDE_H_
 
+#include "ros/ros.h"
+#include "std_srvs/Empty.h"
+#include "mavros_msgs/OverrideRCIn.h"
+
 class RcOverride
 {
+	RcOverride();
+	virtual ~RcOverride();
 
+	bool overrideCallback(
+			std_srvs::Empty::Request& request,
+			std_srvs::Empty::Response& response);
+	void setOverridePublisher(ros::Publisher pub);
 };
 
 #endif /* RCOVERRIDE_H_ */
--- nodes/rc_override_node.cpp
+++ nodes/rc_override_node.cpp
@@ -1,5 +1,14 @@
+#include "RcOverride.h"
 
+/**
+ *	Main override node.
+ */
 int main (int argc, char **argv)
 {
+	ros::init(argc, argv, "rc_override_node");
+	ros::NodeHandle nh;
 
+	RcOverride rcOverride;
+	ros::Publisher overridePub = nh.advertise<mavros_msgs::OverrideRCIn>(
+			"/mavros/rc/override", 1);
 }
